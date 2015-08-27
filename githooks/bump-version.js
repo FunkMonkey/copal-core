55c3248036050e627e5904a451be57240d543c9f
AndSelm <and.selm+github@gmail.com>
2015-08-27 17:38:54 +0200

    MAINT: refactoring git hooks

---
+++ githooks/bump-version.js
@@ -0,0 +1,16 @@
+var hookUtils = require("copal-git-hook-utils");
+
+// Only on these branches
+var CHECKED_BRANCHES = ["master", "dev", "development", "devel"];
+hookUtils.branches.exitIfCurrentBranchIsNot( CHECKED_BRANCHES, 0 );
+
+// Bump, if necessary
+var lastCommits = hookUtils.commits.getCommitSubjectsSince( hookUtils.version.getLastVersionTag() );
+
+if( lastCommits.length === 0 || hookUtils.commitMessage.hasType( lastCommits[0], "VERSION" ) )
+  process.exit( 0 );
+
+var biggest = hookUtils.version.getBiggestVersionType( lastCommits );
+if( biggest !== "version" ) {
+  hookUtils.version.bumpVersion( biggest );
+}
