97b6f1c07b5d3eb2f45eef37c2265d0af4924426
AndSelm <and.selm+git@gmail.com>
2017-11-19 16:46:25 +0100

    FIX: merging `filter` calls of operator `getCommandNames`

--- src/basic-operators.js
+++ src/basic-operators.js
@@ -7,8 +7,8 @@ export default function ( copal ) {
       query$
         .map( query => query.toLowerCase() )
         .map( query => R.values( copal.getGraphTemplates() )
-          .filter( template => !template.hidden )
-          .filter( template => template.name.toLowerCase().indexOf( query ) > -1 )
+          .filter( template => !template.hidden &&
+                               template.name.toLowerCase().indexOf( query ) > -1 )
           .map( template => template.name ) ),
 
     'core.executeCommandGraph': ( [ commandName$ ] ) =>
